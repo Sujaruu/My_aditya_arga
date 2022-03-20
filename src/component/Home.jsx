a7723b14eb0d81e2d8f1a9fd6d5e811deb304a9b
Sujaruu <93075183+Sujaruu@users.noreply.github.com>
2022-03-20 15:43:53 +0700

    editing link cta

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -11,13 +11,13 @@ const Home = () => {
         <h2 className="text-4xl sm:text-7xl font-bold text-[#276678]">I'm a FrontEnd Developer</h2>
         <p className="text-[#557B83] py-4 max-w-[700px]">I'm a FrontEnd Developer Specializing in create a web interface and usually make my own design , i'm currently focused on buildding a responsive web applications</p>
         <div>
-          <button className="text-[#276678] group border-2 border-[#276678] px-6 py-3 my-2 flex items-center hover:bg-[#1687A7] hover:text-white">
-            <Link activeClass="active" to="works" smooth={true} offset={-70} duration={500}>
+          <button>
+            <Link activeClass="active" to="works" smooth={true} offset={-70} duration={500} className="text-[#276678] group border-2 border-[#276678] px-6 py-3 my-2 flex items-center hover:bg-[#1687A7] hover:text-white">
               View Works
+              <span className="group-hover:rotate-90 duration-300">
+                <FiArrowRight className="ml-3" />
+              </span>
             </Link>
-            <span className="group-hover:rotate-90 duration-300">
-              <FiArrowRight className="ml-3" />
-            </span>
           </button>
         </div>
       </div>
